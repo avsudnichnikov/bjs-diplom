6074e6ef86f4f9c5fe8f1a8761f868f549130e53
avsudnichnikov <avsudnichnikov@gmail.com>
2020-11-18 00:48:49 +0500

    add moneyManager methods

--- public/homePage.js
+++ public/homePage.js
@@ -1,21 +1,60 @@
 'use strict';
 
 const logoutBtn = new LogoutButton();
+const ratesBoard = new RatesBoard();
+const moneyManager = new MoneyManager();
+
+
+
+const logoutApiConnectorCallback = (response) => {
+    if (response.success) {
+        window.location.reload();
+    }
+}
 
 logoutBtn.action = () => {
     ApiConnector.logout(logoutApiConnectorCallback);
 };
 
-const logoutApiConnectorCallback = (res) => {
-    if (res.success) {
-        window.location.reload();
-    }
-}
-
-const currentUserApiConnectorCallback = (res) => {
-    if (res.success) {
-        ProfileWidget.showProfile(res.data);
+const currentUserApiConnectorCallback = (response) => {
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);
     }
 };
 
 ApiConnector.current(currentUserApiConnectorCallback);
+
+const getStocksApiConnectorCallback = (response) => {
+    if (response.success) {
+        ratesBoard.clearTable();
+        ratesBoard.fillTable(response.data);
+    }
+};
+
+const getRates = () => {
+    ApiConnector.getStocks(getStocksApiConnectorCallback);
+};
+
+getRates();
+setInterval(getRates, 60 * 1000);
+
+const moneyManagerApiConnectorCallback = (response) => {
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);
+        moneyManager.setMessage(true, 'Успешно!');
+    } else {
+        moneyManager.setMessage(false, response.error);
+    }
+};
+
+moneyManager.addMoneyCallback = (data) => {
+    ApiConnector.addMoney(data, moneyManagerApiConnectorCallback);
+};
+
+moneyManager.conversionMoneyCallback = (data) => {
+    ApiConnector.convertMoney(data, moneyManagerApiConnectorCallback);
+};
+
+moneyManager.sendMoneyCallback = (data) => {
+    ApiConnector.transferMoney(data, moneyManagerApiConnectorCallback);
+};
--- public/loginPage.js
+++ public/loginPage.js
@@ -3,25 +3,21 @@
 const userForm = new UserForm();
 
 userForm.loginFormCallback = (data) => {
-    ApiConnector.login(data, loginApiConnectorCallback);
-};
-
-const loginApiConnectorCallback = (res) => {
-    if (res.success) {
-        window.location.reload();
-    } else {
-        userForm.setLoginErrorMessage('Авторизация не удалась');
-    }
+    ApiConnector.login(data, (response) => {
+        if (response.success) {
+            window.location.reload();
+        } else {
+            userForm.setLoginErrorMessage('Авторизация не удалась');
+        }
+    });
 };
 
 userForm.registerFormCallback = (data) => {
-    ApiConnector.login(data, registerApiConnectorCallback);
-};
-
-const registerApiConnectorCallback = (res) => {
-    if (res.success) {
-        window.location.reload();
-    } else {
-        userForm.setRegisterErrorMessage('Регистрация не удалась');
-    }
+    ApiConnector.login(data, (response) => {
+        if (response.success) {
+            window.location.reload();
+        } else {
+            userForm.setRegisterErrorMessage('Регистрация не удалась');
+        }
+    });
 };
